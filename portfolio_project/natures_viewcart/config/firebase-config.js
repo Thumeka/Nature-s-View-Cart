52b506ba790b189b0171b236aa0ce59953372916
thumeka <thumekatembani21@gmail.com>
2024-05-14 21:53:46 +0200

    install json server for mock API

--- portfolio_project/natures_viewcart/config/firebase-config.js
+++ portfolio_project/natures_viewcart/config/firebase-config.js
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSSKAF5L0rFApG7j_JrwKaw3H8LtnIiA4",
-  authDomain: "nature-s-cart.firebaseapp.com",
-  projectId: "nature-s-cart",
-  storageBucket: "nature-s-cart.appspot.com",
-  messagingSenderId: "168210363008",
-  appId: "1:168210363008:web:df8900ff5b8c6c9c8aec65",
-  measurementId: "G-ZF2G0CRQLG"
+  apiKey: 'AIzaSyCSSKAF5L0rFApG7j_JrwKaw3H8LtnIiA4',
+  authDomain: 'nature-s-cart.firebaseapp.com',
+  projectId: 'nature-s-cart',
+  storageBucket: 'nature-s-cart.appspot.com',
+  messagingSenderId: '168210363008',
+  appId: '1:168210363008:web:df8900ff5b8c6c9c8aec65',
+  measurementId: 'G-ZF2G0CRQLG'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Firestore and Auth
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { firestore, auth };
